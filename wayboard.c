fc81aff68a55ee5e9e8a628f77d9afd8c4a4ad65
woofdoggo <46545045+woofdoggo@users.noreply.github.com>
2023-06-03 00:09:54 -0400

    Do not assert with no config file

--- wayboard.c
+++ wayboard.c
@@ -476,6 +476,7 @@ main(int argc, char **argv) {
 
     if (argc != 2) {
         fprintf(stderr, "USAGE: %s CONFIG_FILE\n", argv[0]);
+        return 1;
     }
     read_config(argv[1]);
     if (config.font != NULL) {
